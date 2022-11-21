551273f30a9a886f9ea5c10182ab73fa0314d109
Nikita Havryliuk <chinter.games1@gmail.com>
2022-11-21 14:05:38 +0200

    start work

--- backend/users/seeding.js
+++ backend/users/seeding.js
@@ -5,12 +5,12 @@ import { faker } from '@faker-js/faker';
 function createRandomUser() {
   const gender = faker.name.sexType();
   const firstName = faker.name.firstName(gender);
-  const friendsId = [];
+  const followerId = [];
 
   return {
     firstName,
     gender,
-    friendsId
+    followerId
   };
 };
 
@@ -23,4 +23,24 @@ for (let i = 1; i <= 200; i++) {
   users.push(user);
 };
 
+function createFollowers(user) {
+  const iterationCount = Math.floor(Math.random() * 150);
+
+  if (iterationCount <= 50) {
+    return user;
+  }
+
+  for (let i = 0; i <= iterationCount; i++) {
+    const randomUserId = Math.floor(Math.random() * 200);
+
+    user.followerId.push(randomUserId);
+  }
+
+  return user;
+}
+
+users.map(user => createFollowers(user));
+
 export default { users };
+
+console.log(users);
